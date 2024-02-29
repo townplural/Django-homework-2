933c85e0abb2069350950cf33e4d5576ca9b7cf2
zitraxmode <bobrenskix.bogdan@gmail.com>
2024-03-01 02:26:01 +0300

    initial commit

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -8,8 +8,16 @@ def index(request):
 
 
 def show_catalog(request):
+    sort_type = request.GET.get("sort")
     template = 'catalog.html'
-    phones_all = Phone.objects.all()
+    if sort_type == 'min_price':
+        phones_all = Phone.objects.all().order_by('price')
+    elif sort_type == 'max_price':
+        phones_all = Phone.objects.all().order_by('-price')
+    elif sort_type == 'name':
+        phones_all = Phone.objects.all().order_by('name')
+    else:
+        phones_all = Phone.objects.all()
     context = {'phones': phones_all}
     return render(request, template, context)
 
@@ -27,9 +35,3 @@ def sorted_cheap(request):
     context = {'phone': phones_all}
     return render(request, template, context)
 
-
-def sorted_expensive(request):
-    template = 'catalog.html'
-    phones_all = Phone.objects.all().order_by('-price')
-    context = {'phone': phones_all}
-    return render(request, template, context)
